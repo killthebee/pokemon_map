50af4aa61fe20b01523473efc0aee68c34ba6636
DANNY <51859323+killthebee@users.noreply.github.com>
2020-02-05 22:04:58 +0300

    create pokemonentity model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,5 +3,19 @@ from django.db import models
 class Pokemon(models.Model):
     title = models.TextField()
     picture = models.ImageField(null=True, blank=True, upload_to='pokemon_pics/')
+
     def __str__(self):
         return self.title
+
+
+class PokemonEntity(models.Model):
+    lat = models.FloatField()
+    lon = models.FloatField()
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+    appeared_at = models.DateTimeField(blank=True, default=None, null=True)
+    disappear_at = models.DateTimeField(blank=True, default=None, null=True)
+    level = models.IntegerField(blank=True, null=True)
+    health = models.IntegerField(blank=True, null=True)
+    strength = models.IntegerField(blank=True, null=True)
+    defence = models.IntegerField(blank=True, null=True)
+    stamina = models.IntegerField(blank=True, null=True)
--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -4,6 +4,8 @@ import json
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
 
+from pokemon_entities.models import Pokemon, PokemonEntity
+
 
 MOSCOW_CENTER = [55.751244, 37.618423]
 DEFAULT_IMAGE_URL = "https://vignette.wikia.nocookie.net/pokemon/images/6/6e/%21.png/revision/latest/fixed-aspect-ratio-down/width/240/height/240?cb=20130525215832&fill=transparent"
@@ -33,11 +35,12 @@ def show_all_pokemons(request):
                 pokemon['title_ru'], pokemon['img_url'])
 
     pokemons_on_page = []
+    pokemons = Pokemon.objects.all()
     for pokemon in pokemons:
         pokemons_on_page.append({
-            'pokemon_id': pokemon['pokemon_id'],
-            'img_url': pokemon['img_url'],
-            'title_ru': pokemon['title_ru'],
+            'pokemon_id': pokemon.id,
+            'img_url': 'media/%s'%(pokemon.picture),
+            'title_ru': pokemon.title,
         })
 
     return render(request, "mainpage.html", context={
